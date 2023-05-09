05a896b2331024a88720af984c64b189eb06965e
Tausif4171 <tausifkhan4171@gmail.com>
2023-05-09 11:17:55 +0530

    created another route for testing nodemon(not need to run every time while the file changes) ✌️

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello Node API')
 })
 
+app.get('/blog', (req, res) => {
+    res.send('Hii')
+})
+
 app.listen(7000, () => {
     console.log('Node API app is running on port 7000')
 })
