a795d1e0122e761fe65bd7569f46d1b8a820cf94
Tausif4171 <tausifkhan4171@gmail.com>
2023-05-09 19:50:06 +0530

    saving data to the database through product model

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose')
 
 app.use(express.json())
 
+const productModel = require('./models/ProductModel')
+
 // routes
 app.get('/', (req, res) => {
     res.send('Hello Node API')
@@ -14,18 +16,25 @@ app.get('/blog', (req, res) => {
     res.send('Hii')
 })
 
-app.post('/product',(req, res)=>{
-    console.log(req.body)
-    res.send(req.body)
+app.post('/products', async (req, res) => {
+    try {
+        const product = await productModel.create(req.body)
+        console.log(product, req.body)
+        res.status(200).json(product)
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ message: error.message })
+    }
 })
 
 mongoose.connect('mongodb+srv://Tausif4171:tausif123@crudinmern.odmk4g1.mongodb.net/CRUDINMERN?retryWrites=true&w=majority')
-.then(()=>{
-    console.log('connected to mongoDB')
-    app.listen(7000, () => {
-        console.log('Node API app is running on port 7000')
+    .then(() => {
+        console.log('connected to mongoDB')
+        app.listen(7000, () => {
+            console.log('Node API app is running on port 7000')
+        })
     })
-})
-.catch((error)=>{
-    console.log(error)
-})
+    .catch((error) => {
+        console.log(error)
+    })
