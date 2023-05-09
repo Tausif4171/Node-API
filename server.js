7cb96b842561581340817643477e905eda2ae89b
Tausif4171 <tausifkhan4171@gmail.com>
2023-05-09 17:51:54 +0530

    firstly connected with database and then run the application

--- server.js
+++ server.js
@@ -13,13 +13,12 @@ app.get('/blog', (req, res) => {
     res.send('Hii')
 })
 
-app.listen(7000, () => {
-    console.log('Node API app is running on port 7000')
-})
-
 mongoose.connect('mongodb+srv://Tausif4171:tausif123@crudinmern.odmk4g1.mongodb.net/CRUDINMERN?retryWrites=true&w=majority')
 .then(()=>{
     console.log('connected to mongoDB')
+    app.listen(7000, () => {
+        console.log('Node API app is running on port 7000')
+    })
 })
 .catch((error)=>{
     console.log(error)
